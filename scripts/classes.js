c3be46e80e271a11195eee2b643d0b4e0c5b9f35
Mostafa <mostafaelkhmisi@hotmail.com>
2021-09-11 20:37:42 +0200

    some tidying

---
+++ scripts/classes.js
@@ -0,0 +1,221 @@
+function randomShotsWithObjectDetection(width, height, x, y, action, target){
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.speedX = 0;
+	this.speedY = 0;
+	this.y = y;
+	this.action = action;
+	this.isAlive=true;
+	this.angle;
+	this.target = target;
+	this.targeting = false;
+	this.dx;
+	this.dy;
+
+	this.currentXSpeed;
+	this.currentYSpeed;
+
+	this.img = new Image();
+	this.img.src = shotImg;
+	this.img.onload = () => {
+		ctx.save();
+		triangleImage(ctx, this.x, this.y, this.width, this.height);
+		ctx.clip();
+		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+		ctx.restore();
+	};
+
+	this.update = function(){
+		action(this);
+		if (this.targeting == false) { 
+			this.currentYSpeed = this.speedY;
+			this.currentXSpeed = this.speedX;
+			this.x += this.speedX;
+			this.y += this.speedY;	
+		}
+		ctx = gameArea.context;
+		ctx.save();
+		triangleImage(ctx, this.x, this.y, this.width, this.height, this.angle)
+		ctx.clip();
+		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+		ctx.restore();
+
+		setTimeout(() => {
+			this.targeting = true;
+			let data = getDistanceAndAngleAndDxDy(this.target.x, this.target.y, this.x, this.y)
+			this.angle = data.angle;
+			this.x += data.dx;
+			this.y += data.dy;
+		}, 500);
+
+		if (this.isAlive == false) {
+			let AllGameObjects = gameObjects.objects[1];
+			if (AllGameObjects != null && running == true) {
+				var minLength = 6000;
+				var minObj = [];
+				origX = this.x;
+				origY = this.y;
+				AllGameObjects.forEach((element, index) => {
+					if(Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2) < minLength){
+						minObj.push(element);
+						minLength = Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2);
+					 }
+				});
+				AllGameObjects.forEach((element1, index) => {
+					minObj.forEach(element2 => {
+						if(element1 == element2){
+							animateParticules(element2.x, element2.y);
+							element2.isAlive=false
+							gameObjects.remove(element2);
+							closestObject = element2
+						}else{ // the rest of objects
+						}
+					});
+
+				});
+			}
+		}
+
+		if (this.target.isAlive == false) {
+			let AllGameObjects = gameObjects.objects[1];
+			if (AllGameObjects != null && running == true) {
+				var minLength = 10000000;
+				var minObj = null;
+				origX = this.x;
+				origY = this.y;
+				AllGameObjects.forEach((element, index) => {
+					if(Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2) < minLength){
+						minObj = element;
+						minLength = Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2);
+					 }
+				});
+				AllGameObjects.forEach((element, index) => {
+					if(element == minObj){
+						this.target = element
+					}else{ // the rest of objects
+					}
+				});
+			}
+
+		}
+
+	}
+	this.isTouching = function(other){
+		let yes = true;
+		if(other.x + other.width -5 < this.x || this.x + this.width -5 < other.x){
+			yes = false;
+		}
+		if(other.y + other.height -5 < this.y || this.y + this.height -5 < other.y){
+			yes = false;
+		}
+		return yes;
+	}
+
+	this.isOnScreen = function(){
+		if(this.x + this.width > 0 || this.x < gameArea.canvas.width){
+			return true;
+		}
+		if(this.y + this.height > 0 || this.y < gameArea.canvas.height){
+			return true;
+		}
+		return false;
+	}
+
+	this.update();
+}
+
+
+
+
+
+
+
+function component(width, height, color, x, y, action, type){
+	this.width = width;
+	this.height = height;
+	this.color = color;
+	this.x = x;
+	this.speedX = 0;
+	this.speedY = 0;
+	this.y = y;
+	this.action = action;
+	this.isAlive=true;
+	this.angle;
+	
+	if (type == "player") {
+		this.img = new Image();
+		this.img.src = planeImg;
+		this.img.onload = () => {
+			ctx.save();
+			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
+			ctx.clip();
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+			ctx.restore();
+
+		};
+	}else if (type == "blocks"){
+		this.img = new Image();
+		this.img.src = bombImg;
+		this.img.onload = () => {
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+		};
+	}else if(type == "upgradeObj"){
+		this.img = new Image();
+		this.img.src = upgradeImg;
+		this.img.onload = () => {
+			ctx.save();
+			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
+			ctx.clip();
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+			ctx.restore();
+		};
+	}
+
+	this.update = function(){
+		action(this);
+		this.x += this.speedX;
+		this.y += this.speedY;
+		ctx = gameArea.context;
+		ctx.fillStyle = color;
+		if (type == "player"){
+			ctx.save();
+			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
+			ctx.clip();
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+			ctx.restore();
+
+		}else if (type == "blocks"){
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+		}else if (type == "upgradeObj"){
+			ctx.save();
+			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
+			ctx.clip();
+			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+			ctx.restore();
+		}
+
+	}
+	this.isTouching = function(other){
+		let yes = true;
+		if(other.x + other.width -5 < this.x || this.x + this.width -5 < other.x){
+			yes = false;
+		}
+		if(other.y + other.height -5 < this.y || this.y + this.height -5 < other.y){
+			yes = false;
+		}
+		return yes;
+	}
+
+	this.isOnScreen = function(){
+		if(this.x + this.width > 0 || this.x < gameArea.canvas.width){
+			return true;
+		}
+		if(this.y + this.height > 0 || this.y < gameArea.canvas.height){
+			return true;
+		}
+		return false;
+	}
+	this.update();
+}
--- scripts/helpers.js
+++ scripts/helpers.js
@@ -26,241 +26,6 @@ function triangleImage(ctx, x, y, width, height, angle) {
 }
 
 
-function component(width, height, color, x, y, action, type){
-	this.width = width;
-	this.height = height;
-	this.color = color;
-	this.x = x;
-	this.speedX = 0;
-	this.speedY = 0;
-	this.y = y;
-	this.action = action;
-	this.isAlive=true;
-	this.angle;
-	
-	if (type == "player") {
-		this.img = new Image();
-		this.img.src = planeImg;
-		this.img.onload = () => {
-			ctx.save();
-			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
-			ctx.clip();
-			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-			ctx.restore();
-
-		};
-	}else if (type == "blocks"){
-		this.img = new Image();
-		this.img.src = bombImg;
-		this.img.onload = () => {
-			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-		};
-	}else if(type == "upgradeObj"){
-		this.img = new Image();
-		this.img.src = upgradeImg;
-		this.img.onload = () => {
-			ctx.save();
-			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
-			ctx.clip();
-			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-			ctx.restore();
-		};
-	}
-
-	this.update = function(){
-		action(this);
-		this.x += this.speedX;
-		this.y += this.speedY;
-		ctx = gameArea.context;
-		ctx.fillStyle = color;
-		if (type == "player"){
-			ctx.save();
-			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
-			ctx.clip();
-			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-			ctx.restore();
-
-		}else if (type == "blocks"){
-			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-
-		}else if (type == "upgradeObj"){
-			ctx.save();
-			roundedImage(ctx, this.x, this.y, this.width, this.height, 20);
-			ctx.clip();
-			ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-			ctx.restore();
-		}
-
-	}
-	this.isTouching = function(other){
-		let yes = true;
-		if(other.x + other.width -5 < this.x || this.x + this.width -5 < other.x){
-			yes = false;
-		}
-		if(other.y + other.height -5 < this.y || this.y + this.height -5 < other.y){
-			yes = false;
-		}
-		return yes;
-	}
-
-	this.isOnScreen = function(){
-		if(this.x + this.width > 0 || this.x < gameArea.canvas.width){
-			return true;
-		}
-		if(this.y + this.height > 0 || this.y < gameArea.canvas.height){
-			return true;
-		}
-		return false;
-	}
-	this.update();
-}
-
-
-
-function randomShotsWithObjectDetection(width, height, x, y, action, target){
-	this.width = width;
-	this.height = height;
-	this.x = x;
-	this.speedX = 0;
-	this.speedY = 0;
-	this.y = y;
-	this.action = action;
-	this.isAlive=true;
-	this.angle;
-	this.target = target;
-	this.targeting = false;
-	this.dx;
-	this.dy;
-
-	this.img = new Image();
-	this.img.src = shotImg;
-	this.img.onload = () => {
-		ctx.save();
-		triangleImage(ctx, this.x, this.y, this.width, this.height);
-		ctx.clip();
-		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-		ctx.restore();
-	};
-
-	this.update = function(){
-		action(this);
-		if (this.targeting == false) {
-			this.x += this.speedX;
-			this.y += this.speedY;	
-		}
-		ctx = gameArea.context;
-		ctx.save();
-		triangleImage(ctx, this.x, this.y, this.width, this.height, this.angle)
-		ctx.clip();
-		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-		ctx.restore();
-
-		setTimeout(() => {
-
-			this.targeting = true;
-			origX = this.target.x;
-			origY = this.target.y;
-			//get the distance between the mouse and the ball on both axes
-			this.dx = (origX - this.x) * .125;
-			this.dy = (origY - this.y) * .125;
-			//calculate the distance this would move ...
-			distance = Math.sqrt(this.dx*this.dx + this.dy*this.dy);
-			//... and cap it at 5px
-			if(distance > 3){
-				this.dx *= 3/distance;
-				this.dy *= 3/distance;
-			}
-			
-			let angle = Math.atan2(this.dy,  this.dx) + 1.6;
-	
-			this.angle = angle;
-			this.x += this.dx;
-			this.y += this.dy;
-		}, 500);
-
-		if (this.isAlive == false) {
-			let AllGameObjects = gameObjects.objects[1];
-			if (AllGameObjects != null && running == true) {
-				var minLength = 6000;
-				var minObj = [];
-				origX = this.x;
-				origY = this.y;
-				AllGameObjects.forEach((element, index) => {
-					if(Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2) < minLength){
-						minObj.push(element);
-						minLength = Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2);
-					 }
-				});
-				AllGameObjects.forEach((element1, index) => {
-					minObj.forEach(element2 => {
-						if(element1 == element2){
-							animateParticules(element2.x, element2.y);
-							element2.isAlive=false
-							gameObjects.remove(element2);
-							closestObject = element2
-						}else{ // the rest of objects
-						}
-					});
-
-				});
-			}
-		}
-
-		if (this.target.isAlive == false) {
-			let AllGameObjects = gameObjects.objects[1];
-			if (AllGameObjects != null && running == true) {
-				var minLength = 10000000;
-				var minObj = null;
-				origX = this.x;
-				origY = this.y;
-				AllGameObjects.forEach((element, index) => {
-					if(Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2) < minLength){
-						minObj = element;
-						minLength = Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2);
-					 }
-				});
-				AllGameObjects.forEach((element, index) => {
-					if(element == minObj){
-						this.target = element
-					}else{ // the rest of objects
-					}
-				});
-			}
-
-		}
-
-	}
-	this.isTouching = function(other){
-		let yes = true;
-		if(other.x + other.width -5 < this.x || this.x + this.width -5 < other.x){
-			yes = false;
-		}
-		if(other.y + other.height -5 < this.y || this.y + this.height -5 < other.y){
-			yes = false;
-		}
-		return yes;
-	}
-
-	this.isOnScreen = function(){
-		if(this.x + this.width > 0 || this.x < gameArea.canvas.width){
-			return true;
-		}
-		if(this.y + this.height > 0 || this.y < gameArea.canvas.height){
-			return true;
-		}
-		return false;
-	}
-
-	this.update();
-}
-
-
-
-
-
-
-
-
 let gameObjects = {
 	objects : {},
 	add : function(obj, layer){
@@ -311,7 +76,6 @@ function updateGameArea() {
 }
 
 
-
 let gameArea = {
 	canvas : document.getElementById("canvas"),
 	start : function(){
@@ -323,8 +87,6 @@ let gameArea = {
 	}
 }
 
-
-
 function clamp(low, high, test) {
 	if(test < low){
 		return low;
@@ -334,11 +96,6 @@ function clamp(low, high, test) {
 	}
 	return test;
 }
-
-
-
-
-
 
 
 function spawnObject(){
@@ -457,4 +214,25 @@ function spawnDetectingShot(){
 	}else{
 		clearInterval(randomShotsUpgrade)
 	}
+}
+
+function getDistanceAndAngleAndDxDy(targetX, targetY, thisX, thisY) {
+	origX = targetX;
+	origY = targetY;
+	//get the distance between the mouse and the ball on both axes
+	let dx = (origX - thisX) * .125;
+	let dy = (origY - thisY) * .125;
+	//calculate the distance this would move ...
+	distance = Math.sqrt(dx*dx + dy*dy);
+	//... and cap it at 3px
+	if(distance > 3){
+		dx *= 3/distance;
+		dy *= 3/distance;
+	}
+	return {
+		distance: distance,
+		angle:  Math.atan2(dy,  dx) + 1.6,
+		dx:dx,
+		dy:dy 
+	  }
 }
