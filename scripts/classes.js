6f3c5450d2a470e099bfbc0c52cb04c28c8a10f9
Mostafa <mostafaelkhmisi@hotmail.com>
2021-09-15 22:40:28 +0200

    code fixes and still trying curve

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,3 +1,5 @@
+
+
 function randomShotsWithObjectDetection(width, height, x, y, action, target){
 	this.width = width;
 	this.height = height;
@@ -13,9 +15,21 @@ function randomShotsWithObjectDetection(width, height, x, y, action, target){
 	this.dx;
 	this.dy;
 	this.dmg = 100;
-
 	this.currentXSpeed;
 	this.currentYSpeed;
+	this.curveDone=false;
+
+
+////////
+	// this.ball = {x:planeX,y:planeY,speed:0.1,t:0};
+	// this.points = [
+	// 	{x:planeX,y:planeY},
+	// 	{x:this.ball.x+30,y:this.ball.y+30},
+	// 	{x:this.ball.x+60,y:this.ball.y+60},
+	// 	{x:this.ball.x+60,y:this.ball.y-30}
+	// ]
+/////////
+
 
 	this.img = new Image();
 	this.img.src = shotImg;
@@ -35,6 +49,54 @@ function randomShotsWithObjectDetection(width, height, x, y, action, target){
 			this.x += this.speedX;
 			this.y += this.speedY;	
 		}
+
+
+
+// if (this.curveDone == false) {
+// 	ctx = gameArea.context;
+// 	ctx.save();
+// 	triangleImage(ctx, this.ball.x, this.ball.y, this.width, this.height, this.angle)
+// 	ctx.clip();
+// 	ctx.drawImage(this.img, this.ball.x, this.ball.y, this.width, this.height);
+// 	ctx.restore();
+
+// 	this.angel = Math.atan2(this.ball.y, this.ball.x) + 1.6,
+// 	setTimeout(() => {
+// 		this.targeting = true;
+// 		let [p0, p1, p2, p3] = this.points;
+// 		//Calculate the coefficients based on where the ball currently is in the animation
+// 		let cx = 3 * (p1.x - p0.x);
+// 		let bx = 3 * (p2.x - p1.x) - cx;
+// 		let ax = p3.x - p0.x - cx - bx;
+	
+// 		let cy = 3 * (p1.y - p0.y);
+// 		let by = 3 * (p2.y - p1.y) - cy;
+// 		let ay = p3.y - p0.y - cy -by;
+	
+// 		let t = this.ball.t;
+	
+// 		//Increment t value by speed
+// 		this.ball.t += 0.01;
+// 		//Calculate new X & Y positions of ball
+// 		let xt = ax*(t*t*t) + bx*(t*t) + cx*t + p0.x;
+// 		let yt = ay*(t*t*t) + by*(t*t) + cy*t + p0.y;
+	
+// 		if(this.ball.t > 1){
+// 			this.ball.t=1;
+// 			this.curveDone = true;
+// 		}
+	
+// 		//We draw the ball to the canvas in the new location
+// 		this.ball.x = xt;
+// 		this.ball.y = yt;
+// 		this.x = xt;
+// 		this.y= yt;
+
+// 	}, 500);
+
+// }
+
+// if (this.curveDone == true) {
 		ctx = gameArea.context;
 		ctx.save();
 		triangleImage(ctx, this.x, this.y, this.width, this.height, this.angle)
@@ -42,12 +104,6 @@ function randomShotsWithObjectDetection(width, height, x, y, action, target){
 		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
 		ctx.restore();
 
-		if (this.dx != null && this.speedX > this.dx) {
-			this.speedX = this.dx;
-		}
-		if (this.dy != null && this.speedY > this.dy) {
-			this.speedY = this.dy;
-		}
 		setTimeout(() => {
 			this.targeting = true;
 			let data = getDistanceAndAngleAndDxDy(this.target.x, this.target.y, this.x, this.y)
@@ -55,7 +111,7 @@ function randomShotsWithObjectDetection(width, height, x, y, action, target){
 			this.x += data.dx;
 			this.y += data.dy;
 		}, 500);
-
+	// }
 
 		if (this.isAlive == false) {
 			let AllGameObjects = gameObjects.objects[2];
@@ -106,10 +162,6 @@ function randomShotsWithObjectDetection(width, height, x, y, action, target){
 				});
 			}
 
-			// let angle = (this.angle - 90) / 180 * Math.PI;  // compensate angle -90°, conv. to rad
-			// let angle = this.angle / 180 * Math.PI;  // compensate angle -90°, conv. to rad
-			// this.x += Math.cos(angle) *3;          // move ship
-			// this.y += Math.sin(angle) *3;
 		}
 
 	}
@@ -155,9 +207,6 @@ function BulletsWithUpgrade(width, height, x, y, action, target){
 	this.dy;
 	this.dmg = 50;
 
-	this.currentXSpeed;
-	this.currentYSpeed;
-
 	this.img = new Image();
 	this.img.src = bulletImage;
 	this.img.onload = () => {
@@ -170,12 +219,8 @@ function BulletsWithUpgrade(width, height, x, y, action, target){
 
 	this.update = function(){
 		action(this);
-		// if (this.targeting == false) { 
-			// this.currentYSpeed = this.speedY;
-			// this.currentXSpeed = this.speedX;
 			this.x += this.speedX;
 			this.y += this.speedY;	
-		// }
 		ctx = gameArea.context;
 		ctx.save();
 		triangleImage(ctx, this.x, this.y, this.width, this.height, this.angle)
@@ -183,37 +228,8 @@ function BulletsWithUpgrade(width, height, x, y, action, target){
 		ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
 		ctx.restore();
 
-		// this.targeting = true;
-		// let data = getDistanceAndAngleAndDxDy(this.target.x, this.target.y, this.x, this.y)
-		// this.angle = data.angle;
-		// this.x += data.dx;
-		// this.y += data.dy;
-
-
-		// if (this.target.isAlive == false) {
-		// 	let AllGameObjects = gameObjects.objects[2];
-		// 	if (AllGameObjects != null && running == true) {
-		// 		var minLength = 10000000;
-		// 		var minObj = null;
-		// 		origX = this.x;
-		// 		origY = this.y;
-		// 		AllGameObjects.forEach((element, index) => {
-		// 			if(Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2) < minLength){
-		// 				minObj = element;
-		// 				minLength = Math.pow(element.x - origX,2) + Math.pow(element.y - origY,2);
-		// 			 }
-		// 		});
-		// 		AllGameObjects.forEach((element, index) => {
-		// 			if(element == minObj){
-		// 				this.target = element
-		// 			}else{ // the rest of objects
-		// 			}
-		// 		});
-		// 	}
-
-		// }
-
 	}
+
 	this.isTouching = function(other){
 		let yes = true;
 		if(other.x + other.width -5 < this.x || this.x + this.width -5 < other.x){
